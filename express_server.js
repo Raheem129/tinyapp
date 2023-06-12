c7385885390c534bb92ece2c6cb6691bf2c75542
Abdi-Raheem <raheem129@hotmail.com>
2023-06-12 16:18:58 -0400

    added get/post route

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+function generateRandomString() {}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -7,7 +9,14 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-};
+}; 
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+}); 
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,6 +34,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 }); 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
