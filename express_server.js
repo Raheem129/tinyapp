97310b7c1df93c18af6e0bbf38073e7cb2fd9f5b
Abdi-Raheem <raheem129@hotmail.com>
2023-06-13 17:23:08 -0400

    added for to url show that submits updated url, add edit button to each object in index.

--- express_server.js
+++ express_server.js
@@ -2,65 +2,61 @@ function generateRandomString() {}
 
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}; 
+};
 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const id = generateRandomString();
-  urlDatabase[id] = longURL;
-  console.log(urlDatabase); // Log the updated urlDatabase to the console
-  res.redirect(`/urls/${id}`);
-}); 
+  const id = generateRandomString(); 
+  const longURL = req.body.longURL; 
+  urlDatabase[id] = longURL; 
+  console.log(urlDatabase); 
+  res.redirect(`/urls/${id}`); 
+});
 
-app.post("/urls/:id/delete", (req, res) => {
+app.post('/urls/:id/delete', (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect('/urls');
 });
- 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.get("/u/:id", (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-}); 
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-}); 
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-}); 
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-}); 
-
+});
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
 app.get("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  const templateVars = { id: id, longURL: longURL };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
