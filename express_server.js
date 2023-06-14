f7f2d775ab28171e25a28708f435590e1345cd0f
Abdi-Raheem <raheem129@hotmail.com>
2023-06-14 14:38:49 -0400

    add user object

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080;
+const users = {};
 
 app.set("view engine", "ejs");
 
@@ -13,22 +14,27 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Function to generate a random string for user ID
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2, 8);
+};
+
 app.post("/urls", (req, res) => {
-  const id = generateRandomString(); 
-  const longURL = req.body.longURL; 
-  urlDatabase[id] = longURL; 
-  console.log(urlDatabase); 
-  res.redirect(`/urls/${id}`); 
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${id}`);
 });
 
 app.post('/urls/:id/delete', (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
   res.redirect('/urls');
-}); 
+});
 
 app.post("/login", (req, res) => {
-  const username = req.body.username; 
+  const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
@@ -36,6 +42,26 @@ app.post("/login", (req, res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
+});
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  // Generate a random ID for the new user
+  const userId = generateRandomString();
+
+  // Create a new user object
+  const newUser = {
+    id: userId,
+    email,
+    password
+  };
+
+  // Save the new user object in the users data store
+  users[userId] = newUser;
+
+  // Redirect to a different page after successful registration
+  res.redirect("/login");
 });
 
 app.get("/", (req, res) => {
@@ -57,28 +83,28 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
-    username: req.cookies.username 
+    user: users[req.cookies.user_id], 
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies.username
+    user: users[req.cookies.user_id],
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies.username
+    user: users[req.cookies.user_id],
   };
   res.render("urls_show", templateVars);
-});  
+});
 
 app.get("/register", (req, res) => {
   res.render("register");
